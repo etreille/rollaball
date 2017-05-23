606948636e337bae0e04b0e3ea33686fd0da4d01
etreille <tonashss@hotmail.fr>
2017-05-23 12:17:00 +0200

    install script fixed

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -9,4 +9,4 @@ curl -o Unity.pkg
 http://netstorage.unity3d.com/unity/2860b30f0b54/MacEditorInstaller/Unity-5.6.1f1.pkg
 
 echo 'Installing Unity.pkg'
-sudo installer -dumplog -package Unity.pkg -target /
+sudo installer -dumplog -package Unity-5.6.1f1.pkg -target /
